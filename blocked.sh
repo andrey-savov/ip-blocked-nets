8513ba8ffe0c854f1e13118313a5d27b1069197c
Andrey Savov <savov@hotmail.com>
2021-01-08 09:04:56 -0800

    Adding DNS records scanners.

--- blocked.sh
+++ blocked.sh
@@ -7,18 +7,20 @@ ipset destroy blocked
 ipset create blocked nethash
 
 ipset -A blocked 5.188.206.0/24
-ipset -A blocked 45.129.32.0/22
+ipset -A blocked 45.129.32.0/22  # IP Volume
 ipset -A blocked 54.39.100.0/23
-ipset -A blocked 74.120.14.0/24
+ipset -A blocked 74.120.14.0/24  # CENSYS-ARIN-02
+ipset -A blocked 78.128.113.0/24  # Miti2000 BG
 ipset -A blocked 84.38.184.0/23
 ipset -A blocked 88.218.17.243
 ipset -A blocked 89.248.168.0/24
 ipset -A blocked 92.63.197.0/24
+ipset -A blocked 94.102.51.0/24  # IP Volume
 ipset -A blocked 141.98.10.0/24
-ipset -A blocked 162.142.125.0/24
-ipset -A blocked 167.248.133.0/24
+ipset -A blocked 162.142.125.0/24  # CENSYS-ARIN-01
+ipset -A blocked 167.248.133.0/24  # CENSYS-ARIN-03
 ipset -A blocked 185.193.91.0/24
-ipset -A blocked 192.35.168.0/23
+ipset -A blocked 192.35.168.0/23  # CENSYS MICH-15324
 ipset -A blocked 193.27.228.0/23
 
 iptables -t raw -A PREROUTING -m set --match-set blocked src -j LOG
